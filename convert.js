27dd99ad17b30486d250a12355cd6f87d46b2be2
Balaji Manickam <balajifolder@gmail.com>
2023-04-28 12:44:07 +0530

    Added video downloader support

--- convert.js
+++ convert.js
@@ -1,26 +1,56 @@
 const config = require("./config.json");
 const fs = require("fs");
 const audioconcat = require("audioconcat");
+const { exec } = require("child_process");
 
 // Convert all audios to single audio file
 function convertAllToOne() {
-  const files = [];
+  const audioFiles = [];
+  const videoFile = fs.createWriteStream(`${config.OUTPUT_FILE_NAME}.txt`);
+
   for (var i = 1; i <= config.LAST_FILE; i++) {
-    files.push(`temp/audio-${i}.aac`);
+    videoFile.write(
+      `file 'file:D:/Projects/discovery-plus-audio-downloader/temp/video/video-${i}.mp4'\n`
+    );
+    audioFiles.push(`temp/audio/audio-${i}.aac`);
   }
-  audioconcat(files)
-    .concat(`${config.OUTPUT_FILE_NAME}.aac`)
-    .on("start", function (command) {
-      console.log("ffmpeg process started:", command);
-    })
-    .on("error", function (err, stdout, stderr) {
-      console.error("Error:", err);
-      console.error("ffmpeg stderr:", stderr);
-    })
-    .on("end", function (output) {
-      fs.rmSync("temp", { recursive: true, force: true });
-      console.error("Audio created in:", output);
-    });
+
+  const isAudioExist = fs.existsSync(`temp/audio`);
+  // if (isAudioExist) {
+  if (false) {
+    fs.unlinkSync(`${config.OUTPUT_FILE_NAME}.aac`);
+    audioconcat(audioFiles)
+      .concat(`${config.OUTPUT_FILE_NAME}.aac`)
+      .on("start", function (command) {
+        console.log("ffmpeg process started:", command);
+      })
+      .on("error", function (err, stdout, stderr) {
+        console.error("Error:", err);
+        console.error("ffmpeg stderr:", stderr);
+      })
+      .on("end", function (output) {
+        fs.rmSync("temp/audio", { recursive: true, force: true });
+        console.error("Audio created in:", output);
+      });
+  }
+
+  const isvideoExist = fs.existsSync(`temp/video`);
+  if (isvideoExist) {
+    try {
+      fs.unlinkSync(`${config.OUTPUT_FILE_NAME}.mp4`);
+    } catch (err) {}
+    const cmd = `ffmpeg -f concat -safe 0 -i ${config.OUTPUT_FILE_NAME}.txt -c copy ${config.OUTPUT_FILE_NAME}.mp4`;
+    console.log(cmd);
+    // exec(cmd, function (err, stdout, stderr) {
+    //   console.log(stdout);
+    //   if (err) console.log(err);
+    //   else {
+    //     // fs.unlinkSync(`${config.OUTPUT_FILE_NAME}.txt`);
+    //     // fs.rmSync("temp/video", { recursive: true, force: true });
+    //     console.log("Video created!");
+    //   }
+    // });
+  }
 }
 
 convertAllToOne();
--- start.js
+++ start.js
@@ -4,44 +4,107 @@ const fs = require("fs");
 
 // Form full files
 function formAllFiles() {
-  const promiseCalls = [[]];
-  const fileLocations = [[]];
-  var j = 1; // To split chuck files
+  const video = {
+    apiCalls: [[]],
+    fileLocations: [[]],
+  };
+  const audio = {
+    apiCalls: [[]],
+    fileLocations: [[]],
+  };
+
+  // To split chuck files
+  var audioCount = 1;
+  var videoCount = 1;
+
   for (var i = 1; i <= config.LAST_FILE; i++) {
-    const fileName = `audio-${i}`;
-    const url = `${config.URL}/${fileName}.aac`;
-    const api = axios({
-      method: "get",
-      url,
-      responseType: "stream",
-    });
-    if (j <= 10) {
-      j++;
-    } else {
-      j = 1;
-      promiseCalls.push([]);
-      fileLocations.push([]);
+    const videoFileName = `video-${i}`;
+    const isVideoNotExist = !fs.existsSync(`temp/video/${videoFileName}.mp4`);
+    if (isVideoNotExist) {
+      console.log(`temp/video/${videoFileName}`);
+      const videoURL = `${config.VIDEO_URL}/${videoFileName}.ts`;
+      const videoApi = axios({
+        method: "get",
+        url: videoURL,
+        responseType: "stream",
+      });
+
+      if (videoCount <= 10) {
+        videoCount++;
+      } else {
+        videoCount = 1;
+        video.apiCalls.push([]);
+        video.fileLocations.push([]);
+      }
+
+      video.apiCalls[video.apiCalls.length - 1].push(videoApi);
+      video.fileLocations[video.fileLocations.length - 1].push(videoFileName);
     }
-    promiseCalls[promiseCalls.length - 1].push(api);
-    fileLocations[fileLocations.length - 1].push(fileName);
+
+    const audioFileName = `audio-${i}`;
+    const isAudioNotExist = !fs.existsSync(`temp/audio/${audioFileName}.aac`);
+    if (isAudioNotExist) {
+      console.log(`temp/audio/${audioFileName}`);
+      const audioURL = `${config.AUDIO_URL}/${audioFileName}.aac`;
+      const audioApi = axios({
+        method: "get",
+        url: audioURL,
+        responseType: "stream",
+      });
+
+      if (audioCount <= 10) {
+        audioCount++;
+      } else {
+        audioCount = 1;
+        audio.apiCalls.push([]);
+        audio.fileLocations.push([]);
+      }
+
+      audio.apiCalls[audio.apiCalls.length - 1].push(audioApi);
+      audio.fileLocations[audio.fileLocations.length - 1].push(audioFileName);
+    }
   }
+
   return {
-    promiseCalls,
-    fileLocations,
+    video,
+    audio,
   };
 }
 
 // Receive full audios
-async function bulkAudioCall({ promiseCalls, fileLocations }) {
-  fs.mkdir("./temp", { recursive: true }, (err) => {
+async function bulkCall({ video, audio }) {
+  fs.mkdir("./temp/video", { recursive: true }, (err) => {
+    if (err) throw err;
+  });
+  fs.mkdir("./temp/audio", { recursive: true }, (err) => {
     if (err) throw err;
   });
   try {
-    promiseCalls.map(async (api, i) => {
+    video.apiCalls.map(async (api, i) => {
       try {
         const results = await Promise.all(api);
         results.forEach((r, j) => {
-          r.data.pipe(fs.createWriteStream(`temp/${fileLocations[i][j]}.aac`));
+          r.data.pipe(
+            fs.createWriteStream(
+              `temp/video/${video.fileLocations[i][j]}.mp4`,
+              {
+                encoding: "utf-8",
+              }
+            )
+          );
+        });
+      } catch (err) {
+        return console.error(err);
+      }
+    });
+
+    audio.apiCalls.map(async (api, i) => {
+      try {
+        const results = await Promise.all(api);
+        results.forEach((r, j) => {
+          r.data.pipe(
+            fs.createWriteStream(`temp/audio/${audio.fileLocations[i][j]}.aac`)
+          );
         });
       } catch (err) {
         return console.error(err);
@@ -54,7 +117,7 @@ async function bulkAudioCall({ promiseCalls, fileLocations }) {
 
 const start = async () => {
   const files = formAllFiles();
-  await bulkAudioCall(files);
+  bulkCall(files);
 };
 
 start();
